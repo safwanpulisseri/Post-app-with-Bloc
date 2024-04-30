7d1aaf8200895fa5d9d92f5aba08e07ca9e6f014
Safwan Pulisseri <safwanpulisseri123@gmail.com>
2024-04-30 19:02:33 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:post_app_with_bloc/application/core/services/theme_services.dart';
+import 'package:post_app_with_bloc/application/features/post/widgets/posts_page.dart';
+import 'package:post_app_with_bloc/theme.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeServiceProvider(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeServiceProvider>(
+        builder: (context, themeServices, child) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode:
+            themeServices.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+        title: 'Flutter Demo',
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        home: const PostPage(),
+      );
+    });
+  }
+}
